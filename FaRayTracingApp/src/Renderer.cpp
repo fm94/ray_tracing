2eed56f634c30cede4c68f65175e9a511a34c0e0
Fares Meghdouri <meghfares@hotmail.com>
2024-05-05 21:28:44 +0200

    ep8

--- FaRayTracingApp/src/Renderer.cpp
+++ FaRayTracingApp/src/Renderer.cpp
@@ -4,19 +4,16 @@
 
 void Renderer::render(const Scene& scene, const Camera& camera)
 {
-	Ray ray;
-	ray.origin = camera.get_position();
+	m_active_scene = &scene;
+	m_active_camera = &camera;
 
 	for (uint32_t y=0; y < m_final_image->GetHeight(); y++)
 	{
 		for (uint32_t x = 0; x < m_final_image->GetWidth(); x++)
 		{
-			ray.direction = camera.get_ray_directions()[x + y * m_final_image->GetWidth()];
-
-			// basically get the color as vec and convert it to 32 bit uint
-			glm::vec4 pixel_color_vec = trace_ray(scene, ray);
-			pixel_color_vec = glm::clamp(pixel_color_vec, glm::vec4(0.0f), glm::vec4(1.0f));
-			m_final_image_data[x + y * m_final_image->GetWidth()] = Utils::vec_to_rgba(pixel_color_vec);
+			glm::vec4 color = ray_gen(x, y);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_final_image_data[x + y * m_final_image->GetWidth()] = Utils::vec_to_rgba(color);
 		}
 	}
 	m_final_image->SetData(m_final_image_data);
@@ -45,17 +42,56 @@ std::shared_ptr<Walnut::Image> Renderer::get_final_image() const
 	return m_final_image;
 }
 
-glm::vec4 Renderer::trace_ray(const Scene& scene, const Ray& ray)
+glm::vec4 Renderer::ray_gen(uint32_t x, uint32_t y)
 {
-	if (scene.spheres.size() == 0)
+	Ray ray;
+	ray.origin = m_active_camera->get_position();
+	ray.direction = m_active_camera->get_ray_directions()[x + y * m_final_image->GetWidth()];
+
+	glm::vec3 color(0.0f);
+	float multiplier = 1.0f;
+
+	int bounces = 2;
+	for (int i = 0; i < bounces; i++)
 	{
-		return glm::vec4(0, 0, 0, 1);
+		Renderer::HitPayload payload = trace_ray(ray);
+		if (payload.hit_distance < 0.0f)
+		{
+			glm::vec3 sky_color = glm::vec3(0.0f, 0.0f, 0.0f);
+			color += sky_color * multiplier;
+			break;
+		}
+
+		// define a light source
+		// think about it in the 3D space
+		glm::vec3 light_src_ray = glm::normalize(glm::vec3(-1, -1, -1));
+		// basically if light ray and normal are in reverse directions we get the best lighting
+		// At 90d we have the least
+		// more than that means we are at the other side so we just clamp with the max op because the dot is negative
+		float light_intensity = glm::max(glm::dot(payload.world_normal, -light_src_ray), 0.0f);
+
+		const Sphere& closest_sphere = m_active_scene->spheres[payload.object_index];
+		// apply intensity on initial color
+		glm::vec3 sphere_color = closest_sphere.albedo * light_intensity;
+		color += sphere_color * multiplier;
+
+		multiplier *= 0.7f;
+
+		ray.origin = payload.world_position + payload.world_normal * 0.0001f;
+		ray.direction = glm::reflect(ray.direction, payload.world_normal);
 	}
 
+	return glm::vec4(color, 1.0f);
+}
+
+Renderer::HitPayload Renderer::trace_ray(const Ray& ray)
+{
 	// look for the closest sphere
-	const Sphere* closest_sphere = nullptr;
+	int closest_sphere = -1;
 	float current_hist_distance = FLT_MAX;
-	for (const Sphere& sphere : scene.spheres) {
+	for (size_t i = 0; i < m_active_scene->spheres.size(); i++) {
+		const Sphere& sphere = m_active_scene->spheres[i];
+
 		// compensate shifted circles (this is recalculated at the end, fix this)
 		glm::vec3 shifted_origin = ray.origin - sphere.position;
 
@@ -75,32 +111,43 @@ glm::vec4 Renderer::trace_ray(const Scene& scene, const Ray& ray)
 		// otherwise we have two solutions, one of them is the one facing our camera
 		// tthe one with the shortest distance is the relevant one here
 		float closest_t = (-b - glm::sqrt(delta_discriminant)) / 2 * a;
-		if (closest_t < current_hist_distance) {
+		// also check if t is negative because that means the object is behind
+		if (closest_t > 0.0f && closest_t < current_hist_distance) {
 			current_hist_distance = closest_t;
-			closest_sphere = &sphere;
+			closest_sphere = (int)i;
 		}
 		//float other_t = (-b + glm::sqrt(delta_discriminant)) / 2 * a;
 	}
 
 	// if nothing has been hit then return black
-	if (closest_sphere == nullptr) {
-		return glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+	if (closest_sphere < 0) {
+		return handle_miss();
 	}
 
-	// solving the equation
-	glm::vec3 shifted_origin = ray.origin - closest_sphere->position;
-	glm::vec3 nearest_hit_point = shifted_origin + ray.direction * current_hist_distance;
-	glm::vec3 nearest_hit_point_normal = glm::normalize(nearest_hit_point); // typically here we consider the origin but it's 0
+	return handle_closest_hit(ray, current_hist_distance, closest_sphere);
+}
 
-	// define a light source
-	// think about it in the 3D space
-	glm::vec3 light_src_ray = glm::normalize(glm::vec3(-1, -1, -1)); 
-	// basically if light ray and normal are in reverse directions we get the best lighting
-	// At 90d we have the least
-	// more than that means we are at the other side so we just clamp with the max op because the dot is negative
-	float light_intensity = glm::max(glm::dot(nearest_hit_point_normal, -light_src_ray), 0.0f);
-	
-	// apply intensity on initial color
-	glm::vec3 sphere_color = closest_sphere->albedo * light_intensity;
-	return glm::vec4(sphere_color, 1.0f);
+Renderer::HitPayload Renderer::handle_closest_hit(const Ray& ray, float hit_distance, int object_index)
+{
+	Renderer::HitPayload payload;
+	payload.hit_distance = hit_distance;
+	payload.object_index = object_index;
+
+	const Sphere& closest_sphere = m_active_scene->spheres[object_index];
+
+	// solving the equation
+	glm::vec3 shifted_origin = ray.origin - closest_sphere.position;
+	// typically here we consider the origin but it's 0
+// update: this is now fixed
+	payload.world_position = shifted_origin + ray.direction * hit_distance;
+	payload.world_normal = glm::normalize(payload.world_position);
+	// shift back but after normalization
+	payload.world_position += closest_sphere.position;
+
+	return payload;
+}
+
+Renderer::HitPayload Renderer::handle_miss()
+{
+	return HitPayload();
 }
--- FaRayTracingApp/src/Renderer.h
+++ FaRayTracingApp/src/Renderer.h
@@ -12,18 +12,29 @@ class Renderer
 {
 public:
 	Renderer() = default;
-
 	void render(const Scene& scene, const Camera& camera);
 	void handle_size(uint32_t width, uint32_t height);
 	std::shared_ptr<Walnut::Image> get_final_image() const;
 
 private:
-	glm::vec4 trace_ray(const Scene& scene, const Ray& ray);
+	glm::vec4 ray_gen(uint32_t x, uint32_t y);
+
+	struct HitPayload
+	{
+		float hit_distance = -1.0f;
+		glm::vec3 world_position;
+		glm::vec3 world_normal;
+		int object_index;
+	};
+
+	HitPayload trace_ray(const Ray& ray);
+	HitPayload handle_closest_hit(const Ray& ray, float hit_distance, int object_index);
+	HitPayload handle_miss();
 
 private:
-
-
 	std::shared_ptr<Walnut::Image> m_final_image;
 	uint32_t* m_final_image_data = nullptr;
+	const Scene* m_active_scene = nullptr;
+	const Camera* m_active_camera = nullptr;
 
 };
